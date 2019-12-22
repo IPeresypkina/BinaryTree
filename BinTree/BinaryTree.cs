99bd1f3da01cdd5c8a78a09d222357ffd18b92a5
IPeresypkina <iperesypkina9@gmail.com>
2019-12-22 16:31:42 +0300

    [feature] add delete value in to binary tree

--- BinTree/BinaryTree.cs
+++ BinTree/BinaryTree.cs
@@ -35,18 +35,106 @@ namespace BinTree
         private BinaryTree<T> _search(BinaryTree<T> tree, T val)
         {
             if(tree == null) return null;
-            switch (val.CompareTo(tree.val)){
-                case 1: return _search(tree.right, val);
-                case -1: return _search(tree.left, val);
-                case 0: return tree;
-                default: return null;
-            }
+            if(val.CompareTo(tree.val) < 0)
+                return _search(tree.left, val);
+            else if (val.CompareTo(tree.val) > 0)
+                return _search(tree.right, val);
+            else return tree;
         }
     
         public BinaryTree<T> search(T val)
         {
             return _search(this, val);
-        }
+        } 
         
+        
+        public bool remove(T val)
+        {
+            //Проверяем, существует ли данный узел
+            BinaryTree<T> tree = search(val);
+            if(tree == null) return false;
+            
+            BinaryTree<T> curTree;
+  
+            //Если удаляем корень
+            if(tree == this)
+            { 
+                if(tree.right!=null) {
+                    curTree = tree.right;
+                }
+                else curTree = tree.left;
+                T temp = curTree.val;
+                this.remove(temp);
+                tree.val = temp;
+                return true;
+            }
+      
+            //Удаление листьев
+            if(tree.left==null && tree.right==null && tree.parent != null){ 
+                if(tree == tree.parent.left)
+                    tree.parent.left = null;
+                else {
+                    tree.parent.right = null;
+                }
+                return true;
+            }
+      
+            //Удаление узла, имеющего левое поддерево, но не имеющее правого поддерева
+            if(tree.left != null && tree.right == null){
+                //Меняем родителя
+                tree.left.parent = tree.parent;
+                if(tree == tree.parent.left){
+                    tree.parent.left = tree.left;
+                }else if(tree == tree.parent.right){
+                    tree.parent.right = tree.left;
+                }
+                return true;
+            }
+      
+            //Удаление узла, имеющего правое поддерево, но не имеющее левого поддерева
+            if(tree.left == null && tree.right != null){
+                //Меняем родителя
+                tree.right.parent = tree.parent;
+                if(tree == tree.parent.left){
+                    tree.parent.left = tree.right;
+                }
+                else if(tree == tree.parent.right){
+                    tree.parent.right = tree.right;
+                }
+                return true;
+            }
+      
+              //Удаляем узел, имеющий поддеревья с обеих сторон
+            if(tree.right!=null && tree.left!=null) {
+                curTree = tree.right;
+      
+                while (curTree.left != null) {
+                    curTree = curTree.left;
+                }
+      
+                //Если самый левый элемент является первым потомком
+                if(curTree.parent == tree) {
+                    curTree.left = tree.left;
+                    tree.left.parent = curTree;
+                    curTree.parent = tree.parent;
+                    tree.parent.left = curTree;
+                }
+                //Если самый левый элемент НЕ является первым потомком
+                else {
+                    if (curTree.right != null) {
+                        curTree.right.parent = curTree.parent;
+                    }
+                    curTree.parent.left = curTree.right;
+                    curTree.right = tree.right;
+                    curTree.left = tree.left;
+                    tree.left.parent = curTree;
+                    tree.right.parent = curTree;
+                    curTree.parent = tree.parent;
+                    tree.parent.right = curTree;
+                }
+                return true;
+            }
+            return false;
+        }
     }
 }
--- BinTree/TestBinary/TreeTest.cs
+++ BinTree/TestBinary/TreeTest.cs
@@ -64,5 +64,128 @@ namespace BinTree.TestBinary
             Assert.AreEqual(null, tree.search(40));
         }
         
+        [Test]
+        public void SearchIntoBinTree()
+        {
+            BinaryTree<int> tree = new BinaryTree<int>(33, null);
+            tree.add(5);
+            tree.add(35);
+            tree.add(1);
+            tree.add(20);
+            tree.add(99);
+            tree.add(17);
+            tree.add(18);
+            tree.add(19);
+            tree.add(31);
+            tree.add(4);
+            Assert.AreEqual(18, tree.search(18).val);
+            Assert.AreEqual(null, tree.search(40));
+        }
+        
+        [Test]
+        public void DeleteLeftValueIntoBinTree()
+        {
+            BinaryTree<int> tree = new BinaryTree<int>(33, null);
+            tree.add(5);
+            tree.add(35);
+            tree.add(1);
+            tree.add(20);
+            tree.add(99);
+            tree.add(17);
+            tree.add(18);
+            tree.add(19);
+            tree.add(31);
+            tree.add(4);
+            Assert.AreEqual(18, tree.search(18).val);
+            Assert.AreEqual(null, tree.search(40));
+        }
+
+        [Test]
+        public void DeleteSheetIntoBinTree() //удаление листа
+        {
+            BinaryTree<int> tree = new BinaryTree<int>(33, null);
+            tree.add(5);
+            tree.add(35);
+            tree.add(1);
+            tree.add(20);
+            tree.add(99);
+            tree.add(34);
+            tree.remove(99); //удаление листа (правого)
+            Assert.AreEqual(null, tree.search(99));
+            tree.remove(1); //удаление листа (левого)
+            Assert.AreEqual(null, tree.search(1));
+        }
+        
+        [Test]
+        public void DeleteNodeHasOneSubtreeIntoBinTree() //удаление узла имеющее одно поддерево
+        {
+            BinaryTree<int> tree = new BinaryTree<int>(33, null);
+            tree.add(5);
+            tree.add(88);
+            tree.add(1);
+            tree.add(20);
+            tree.add(99);
+            tree.add(70);
+            tree.add(17);
+            tree.add(18);
+            tree.add(19);
+            tree.add(60);
+            tree.add(63);
+            tree.add(65);
+            tree.remove(20); //Удаление узла, имеющего левое поддерево, но не имеющее правого поддерева
+            Assert.AreEqual(null, tree.search(20));
+            tree.remove(17); //Удаление узла, имеющего правое поддерево, но не имеющее левого поддерева
+            Assert.AreEqual(null, tree.search(17));
+            tree.remove(70); //Удаление узла, имеющего левое поддерево, но не имеющее правого поддерева
+            Assert.AreEqual(null, tree.search(70));
+            tree.remove(60); //Удаление узла, имеющего правое поддерево(, но не имеющее левого поддерева
+            Assert.AreEqual(null, tree.search(60));
+        }
+        
+        [Test]
+        public void DeleteNodeHasBouthSubtreeIntoBinTree() //удаление узла имеющее оба поддерева
+        {
+            BinaryTree<int> tree = new BinaryTree<int>(33, null);
+            tree.add(5);
+            tree.add(35);
+            tree.add(1);
+            tree.add(99);
+            tree.add(97);
+            tree.add(98);
+            tree.add(31);
+            tree.add(4);
+            tree.add(34);
+            tree.add(100);
+            //Удаляем узел, имеющий поддеревья с обеих сторон
+            //Первый случай (Правое поддерево имеет своих потомков.)
+            tree.remove(35); 
+            Assert.AreEqual(null, tree.search(35));
+            //Второй случай (Правое поддерево не имеет потомка.)
+            tree.remove(5); 
+            Assert.AreEqual(null, tree.search(5));
+        }
+        
+        [Test]
+        public void DeleteNonexistentNodeIntoBinTree() //удаляем не существующий узел
+        {
+            BinaryTree<int> tree = new BinaryTree<int>(33, null);
+            tree.add(5);
+            tree.add(35);
+            tree.add(1);
+            Assert.AreEqual(false, tree.remove(2));
+        }
+        
+        [Test]
+        public void DeleteRootIntoBinTree() //удаляем корень
+        {
+            BinaryTree<int> tree = new BinaryTree<int>(33, null);
+            tree.add(5);
+            tree.add(1);
+            tree.add(35);
+            tree.remove(33); 
+            Assert.AreEqual(null, tree.search(33));
+            tree.remove(35); 
+            Assert.AreEqual(null, tree.search(35));
+        }
     }
 }
